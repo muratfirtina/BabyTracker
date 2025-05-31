6586aba4df06dad87c3e97aac826b28dc60f841e
muratfirtina <muratfirtina@hotmail.com>
2025-05-31 14:50:54 +0300

    Initial Commit

---
+++ BabyTracker/BabyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyTrackerApp.swift
+//  BabyTracker
+//
+//  Created by Murat FIRTINA on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BabyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyTrackerTests/BabyTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BabyTrackerTests.swift
+//  BabyTrackerTests
+//
+//  Created by Murat FIRTINA on 31.05.2025.
+//
+
+import Testing
+@testable import BabyTracker
+
+struct BabyTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
